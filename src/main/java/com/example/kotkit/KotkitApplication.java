3ad1b5999286d19ad91af85392b4373e9ca48635
Tín Vương Nhật <tinvuongnhat@gmail.com>
2024-10-19 18:24:06 +0700

    init

---
+++ src/main/java/com/example/kotkit/KotkitApplication.java
@@ -0,0 +1,13 @@
+package com.example.kotkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotkitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kotkit/KotkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kotkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotkitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
