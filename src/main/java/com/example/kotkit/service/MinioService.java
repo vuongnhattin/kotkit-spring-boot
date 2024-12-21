3286eaec6edd0d059872295646c2bb99311cd445
TranKietHCMUS <anhkiet10h1nd@gmail.com>
2024-12-21 19:01:29 +0700

    add upload video feature

--- src/main/java/com/example/kotkit/service/MinioService.java
+++ src/main/java/com/example/kotkit/service/MinioService.java
@@ -1,75 +1,92 @@
 package com.example.kotkit.service;
 
-import com.example.kotkit.dto.input.VideoInput;
-import com.example.kotkit.dto.response.VideoResponse;
-import com.example.kotkit.entity.Users;
-import com.example.kotkit.entity.Video;
+import com.example.kotkit.exception.AppException;
 import io.minio.*;
+import jakarta.annotation.PostConstruct;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.modelmapper.ModelMapper;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class MinioService {
-    @Autowired
-    private MinioClient minioClient;
-
-    @Autowired
-    private VideoService videoService;
+    private final MinioClient minioClient;
 
     @Value("${minio.bucket}")
     private String bucketName;
 
-    @Value("${minio.endpoint}")
-    private String endpoint;
+    @PostConstruct
+    private void init() {
+        createBucket(bucketName);
+    }
 
-    private final ModelMapper mapper;
+    @SneakyThrows
+    private void createBucket(final String name) {
+        final var found = minioClient.bucketExists(
+                BucketExistsArgs.builder()
+                        .bucket(name)
+                        .build()
+        );
+        if (!found) {
+            minioClient.makeBucket(
+                    MakeBucketArgs.builder()
+                            .bucket(name)
+                            .build()
+            );
 
-    public VideoResponse uploadVideo(MultipartFile file, VideoInput input) {
-        try {
-            // Kiểm tra bucket có tồn tại không
-            boolean bucketExists = minioClient.bucketExists(BucketExistsArgs.builder()
-                    .bucket(bucketName)
-                    .build());
-
-            // Tạo bucket nếu chưa tồn tại
-            if (!bucketExists) {
-                minioClient.makeBucket(MakeBucketArgs.builder()
-                        .bucket(bucketName)
-                        .build());
-            }
-
-            // Tạo tên file ngẫu nhiên để tránh trùng lặp
-            String fileName = UUID.randomUUID().toString() + getFileExtension(file.getOriginalFilename());
-
-            // Upload file lên MinIO
-            minioClient.putObject(PutObjectArgs.builder()
-                    .bucket(bucketName)
-                    .object(fileName)
-                    .stream(file.getInputStream(), file.getSize(), -1)
-                    .contentType(file.getContentType())
-                    .build());
-
-            String fileUrl = endpoint + "/" + bucketName + "/" + fileName;
-
-            Video video = mapper.map(input, Video.class);
-
-            video.setVideoUrl(fileUrl);
-
-            return videoService.uploadVideo(video);
-
-        } catch (Exception e) {
-            log.error("Error uploading file to MinIO", e);
-            throw new RuntimeException("Could not upload file to MinIO", e);
+            final var policy = """
+                        {
+                          "Version": "2012-10-17",
+                          "Statement": [
+                           {
+                              "Effect": "Allow",
+                              "Principal": "*",
+                              "Action": "s3:GetObject",
+                              "Resource": "arn:aws:s3:::%s/*"
+                            }
+                          ]
+                        }
+                    """.formatted(name);
+            minioClient.setBucketPolicy(
+                    SetBucketPolicyArgs.builder().bucket(name).config(policy).build()
+            );
+        } else {
+            log.info("Bucket %s already exists.".formatted(name));
         }
+    }
+
+    @SneakyThrows
+    public String upload(@NonNull final MultipartFile file) {
+        final var fileName = UUID.randomUUID().toString();
+        try {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(fileName)
+                            .contentType(Objects.isNull(file.getContentType()) ? "image/png; image/jpg;" : file.getContentType())
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .build()
+            );
+        } catch (Exception ex) {
+            log.error("Error saving file \n {} ", ex.getMessage());
+            throw new AppException(400, "Unable to upload file");
+        }
+         return minioClient.getPresignedObjectUrl(
+                io.minio.GetPresignedObjectUrlArgs.builder()
+                        .method(io.minio.http.Method.GET)
+                        .bucket(bucketName)
+                        .object(fileName)
+                        .build()
+         );
     }
 
     private String getFileExtension(String fileName) {
